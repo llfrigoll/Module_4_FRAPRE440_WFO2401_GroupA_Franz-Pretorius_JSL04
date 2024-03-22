a89a32cd894c839eacc53c5e936ca059911de7cd
Franz <franzj.pretorius.2003@gmail.com>
2024-03-22 08:45:25 +0200

    Completed challenge 2

--- index.js
+++ index.js
@@ -25,7 +25,11 @@ switch (userPreference) {
 // Use a ternary operator to print "Time to restock!" if the number is less than 5, or "We're stocked!" if the number is 5 or more.
 
 let cansLeft = 3; // Any number of cans
-console.log(``);
+
+//Ternary operation evaluating cansLeft and displaying the correct result
+cansLeft < 5 ? console.log("Time to restock!"): console.log("We're stocked!");
+
+
 
 
 // #### Challenge 3: Workout Intensity
